25881ecbd1f108b2bb65906ece67aba988169c2f
Drystan-Furor <artstristan@gmail.com>
2022-06-24 16:23:01 +0200

    Initialize Multi user todo api

---
+++ sql/schema/user_roles.sql
@@ -0,0 +1,10 @@
+create table USER_ROLES (
+                            USER_ID uuid,
+                            AUTHORITY varchar(225)
+);
+alter table USER_ROLES
+    add constraint USER_ROLES_PK primary key (USER_ID, AUTHORITY);
+alter table USER_ROLES
+    add constraint USER_ROLES_FK_1 foreign key (USER_ID) references USERS (ID) on delete cascade;
+create index USER_ROLES_FK_1_I
+    on USER_ROLES (USER_ID)
---
+++ sql/schema/users.sql
@@ -0,0 +1,30 @@
+create table USERS (
+                         ID            uuid,
+                         FIRST_NAME    varchar(255),
+                         INFIX         varchar(15),
+                         LAST_NAME     varchar(255),
+                         EMAIL         varchar(255) unique                    not null,
+                         PASSHASH      varchar(1024)                          not null,
+
+                         DATE_OF_BIRTH date,
+                         GENDER        varchar(8),
+
+                         STREET        varchar(255),
+                         STREET_NUMBER varchar(255),
+                         CITY          varchar(255),
+                         MUNICIPALITY  varchar(255),
+                         PROVINCE      varchar(255),
+                         COUNTRY_CODE  varchar(2),
+                         STATE         varchar(255),
+                         LATITUDE      varchar(255),
+                         LONGITUDE     varchar(255),
+
+                         CREATED_ON    timestamp with time zone default NOW() not null,
+                         LAST_UPDATED  timestamp with time zone default NOW() not null,
+                         DISABLED_ON   timestamp with time zone
+);
+alter table USERS
+    add constraint USERS_PK primary key (ID);
+create index USERS_I
+    on USERS (DISABLED_ON);
+

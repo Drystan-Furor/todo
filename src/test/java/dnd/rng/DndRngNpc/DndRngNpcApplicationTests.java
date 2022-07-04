7d40cea0fa0aa4514f779e05ef86a4f68de1b0bc
Drystan-Furor <artstristan@gmail.com>
2022-07-04 14:31:08 +0200

    restart project
    reset files

---
+++ src/test/java/dnd/rng/DndRngNpc/DndRngNpcApplicationTests.java
@@ -0,0 +1,13 @@
+package dnd.rng.DndRngNpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndRngNpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
